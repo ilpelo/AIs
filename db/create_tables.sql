845782fbe3e193a8bded9e6893ca01767f1c6733
ilpelo <git@leggere.com>
2015-01-31 00:20:28 +0000

    load lrit

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,9 +1,20 @@
+drop table position;
+
 create table position
 (
 mmsi varchar(15) not null,
 ts int not null,
 lat double not null,
 lon double not null,
+source varchar(1),
 index index_mmsi (mmsi),
 index index_ts (ts)
+);
+
+create table shiptype
+(
+mmsi varchar(15) not null,
+shiptype int not null,
+index index_mmsi (mmsi),
+index index_shiptype (shiptype)
 );
