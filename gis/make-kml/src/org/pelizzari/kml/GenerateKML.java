0afbd78c33df3baf325f29722e0c50fc47f80578
ilpelo <git@leggere.com>
2015-02-01 17:32:44 +0000

    getting tracks of ships making a specific voyage

--- gis/make-kml/src/org/pelizzari/kml/GenerateKML.java
+++ gis/make-kml/src/org/pelizzari/kml/GenerateKML.java
@@ -21,6 +21,72 @@ public class GenerateKML {
 		Statement stmt;
 		ResultSet rs;
 		
+		final String FIRST_DEPARTURE_DAY = "'2011-03-02' ";
+		final String LAST_DEPARTURE_DAY = "'2011-03-03' ";
+		
+		final String DEPARTURE_PERIOD_COND = 
+				"and date(from_unixtime(ts)) >= "+FIRST_DEPARTURE_DAY+
+				"and date(from_unixtime(ts)) < "+LAST_DEPARTURE_DAY;
+
+		final String ARRIVAL_PERIOD_COND = 
+				"and date(from_unixtime(ts)) >= ("+FIRST_DEPARTURE_DAY+ " + INTERVAL 10 DAY) "+
+				"and date(from_unixtime(ts)) < ("+LAST_DEPARTURE_DAY+ " + INTERVAL 10 DAY) ";
+		
+		final String VOYAGE_PERIOD_COND = 
+				"and date(from_unixtime(ts)) >= "+FIRST_DEPARTURE_DAY+
+				"and date(from_unixtime(ts)) < ("+LAST_DEPARTURE_DAY+ " + INTERVAL 10 DAY) ";		
+
+		final String GIBRALTAR_COND = 
+				"and lat between 30 and 40 "+
+				"and lon between -15 and -5 ";
+		
+		final String NEWYORK_COND = 
+				"and lat between 40 and 41 "+
+				"and lon between -75 and -73 ";
+		
+		// select ships with more positions in area/period
+		final String SHIP_DEPARTURE_QUERY = 
+				"SELECT mmsi, count(*) "+
+				"FROM wpos "+
+			    "WHERE 1=1 "+
+			    DEPARTURE_PERIOD_COND +
+				GIBRALTAR_COND +
+				//NEWYORK_COND +
+				"group by mmsi order by 2 desc limit 10";
+
+		final String SHIP_ARRIVAL_QUERY = 
+				"SELECT mmsi "+
+				"FROM wpos "+
+			    "WHERE 1=1 "+
+			    NEWYORK_COND+
+			    ARRIVAL_PERIOD_COND;			    
+
+		// select ships that made the specific voyage
+		final String SHIP_VOYAGE_QUERY = 
+				"SELECT mmsi "+
+				"FROM wpos "+
+			    "WHERE 1=1 "+
+			    NEWYORK_COND+
+			    ARRIVAL_PERIOD_COND+
+			    "and mmsi in ("+
+					"SELECT mmsi "+
+					"FROM wpos "+
+				    "WHERE 1=1 "+
+				    DEPARTURE_PERIOD_COND +
+					GIBRALTAR_COND+
+					")";
+		
+		final String TRACK_QUERY_SELECT_FROM_WHERE = 
+				"SELECT mmsi, ts, lat, lon "+
+				"FROM wpos "+
+			    "WHERE 1=1 "+
+			    VOYAGE_PERIOD_COND;
+		
+		final String TRACK_QUERY_ORDER_LIMIT = 
+				" order by ts desc limit 1000";
+
+		final String OUTPUT_FILE = "c:/master_data/PlaceMarkers.kml";
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://localhost:3306/ai";
@@ -33,20 +99,27 @@ public class GenerateKML {
 			stmt = con.createStatement();
 			// get top 10 ships
 			List<String> top10Ships = new ArrayList<String>();
-			rs = stmt.executeQuery("SELECT mmsi, count(*) FROM position group by mmsi order by 2 desc limit 10");
+			String shipQuery = SHIP_VOYAGE_QUERY;
+			System.out.println(shipQuery);
+			rs = stmt.executeQuery(shipQuery);
 			while(rs.next()){
 				String mmsi = rs.getString("mmsi");
 				top10Ships.add(mmsi);
 			}
 
+			// get track of selected ships and write into kml as a line
 			Iterator<String> itr = top10Ships.iterator();
 			while(itr.hasNext()) {
-				rs = stmt.executeQuery("SELECT * FROM position where mmsi = "+itr.next()+			
-						 " order by ts asc");
+				String mmsi = itr.next();
+				String trackQuery =
+						TRACK_QUERY_SELECT_FROM_WHERE +
+						"and mmsi = "+mmsi+			
+						TRACK_QUERY_ORDER_LIMIT;
+				System.out.println(trackQuery);
+				rs = stmt.executeQuery(trackQuery);
 				ShipPosition pos = null;
 				List<ShipPosition> posList = new ArrayList<ShipPosition>();
 				while(rs.next()){
-					String mmsi = rs.getString("mmsi");
 					float lat = rs.getFloat("lat");
 					float lon = rs.getFloat("lon");
 					pos = new ShipPosition(lat, lon);
@@ -60,7 +133,7 @@ public class GenerateKML {
 			
 			Source src = new DOMSource(kmlGenerator.getDoc());
 			
-			Result dest = new StreamResult(new File("c:/master_data/PlaceMarkers.kml")); 
+			Result dest = new StreamResult(new File(OUTPUT_FILE)); 
 			TransformerFactory tranFactory = TransformerFactory.newInstance(); 
 		    Transformer aTransformer = tranFactory.newTransformer(); 
 			aTransformer.transform(src, dest);
