8372715c0a0b43318f5cbb742ffd00460a361b74
ilpelo <git@leggere.com>
2015-06-15 19:49:06 +0100

    print maps to image file

--- PilotAI/src/org/pelizzari/ai/BestStatistics.java
+++ PilotAI/src/org/pelizzari/ai/BestStatistics.java
@@ -31,6 +31,9 @@ public class BestStatistics extends Statistics {
 	public static final int GEN_OUTPUT_RATE = 3000; // print log every Nth
 													// generations
 	public static Map map, map1;
+	// the filename where the final map is saved
+	public static final String P_IMGFILE = "image-file";
+	File imageFile;
 
 	public void setup(final EvolutionState state, final Parameter base) {
 		// DO NOT FORGET to call super.setup(...) !!
@@ -64,6 +67,21 @@ public class BestStatistics extends Statistics {
 		// read max generation number
 		genMax = state.parameters.getInt(new Parameter("generations"), null);
 		state.output.println("generations: " + genMax, popLog);
+		
+		
+		// build filename to print the map, use crossover/mutation prob and selection
+		// and tournament size
+		int tournSize = state.parameters.getInt(new Parameter("select.tournament.size"), null);
+		Double crossoverLikelihood = state.parameters.getDouble(new Parameter("pop.subpop.0.species.pipe.source.0.likelihood"), null);
+		Double mutationProb = state.parameters.getDouble(new Parameter("pop.subpop.0.species.mutation-prob"), null);
+		String fileName = state.parameters.getString(base.push(P_IMGFILE), null)+
+						"_gen_"+genMax+
+						"_cross_"+String.format("%2.2f", crossoverLikelihood)+
+						"_mut_"+String.format("%2.2f", mutationProb)+
+						"_tourn_"+tournSize+
+						".png";
+		imageFile = new File(fileName);
+		state.output.println("Map image: " + imageFile.getAbsolutePath(), popLog);	
 		// init Map and show target track
 		map = new Map();
 		map1 = new Map();
@@ -117,6 +135,8 @@ public class BestStatistics extends Statistics {
 				if(lastGen) {
 					map1.plotTrack(bestTrack, Color.PINK, ""+state.generation);
 					map1.setVisible(true);
+					// set up imageFile					
+					map1.saveAsImage(imageFile);
 					try {
 						System.in.read();
 					} catch (IOException e) {
